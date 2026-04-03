e200f6e4d4630c059f099c67b39039b8fab2ce2a
Raul <raulgc2995@gmail.com>
2026-04-03 12:07:05 +0200

    add: img in product-page component

--- src/app/products/components/card/card.ts
+++ src/app/products/components/card/card.ts
@@ -3,11 +3,10 @@ import { RouterLink } from '@angular/router';
 import { Product } from '../../interfaces/product.interface';
 import { SlicePipe } from '@angular/common';
 import { ProductImagePipe } from '../../pipes/product-image.pipe';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'card',
-  imports: [RouterLink, SlicePipe, ProductImagePipe, NgOptimizedImage],
+  imports: [RouterLink, SlicePipe, ProductImagePipe],
   templateUrl: './card.html',
 })
 export class Card {
